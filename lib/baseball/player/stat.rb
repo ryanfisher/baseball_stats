495c98b265357da57a658cf0c5df7e3663671460
Ryan Fisher <ryancfisher@gmail.com>
2015-02-15 19:41:16 -0800

    Add comparison and addition to hr and rbi

--- lib/baseball/player/stat.rb
+++ lib/baseball/player/stat.rb
@@ -1,9 +1,17 @@
 module Baseball
   class Player
     class Stat
-      attr_reader :stat
+      attr_reader :value
       def initialize(stat)
-        @stat = stat
+        @value = stat.to_i
+      end
+
+      def betterThan?(other)
+        value > other.value
+      end
+
+      def +(other)
+        self.class.new(value + other.value)
       end
     end
   end
--- spec/baseball/hitter/rbi_spec.rb
+++ spec/baseball/hitter/rbi_spec.rb
@@ -10,4 +10,31 @@ describe Baseball::Hitter::Rbi do
       expect(subject).to be_instance_of described_class
     end
   end
+
+  describe '#value' do
+    it 'gives the value of an instance' do
+      expect(subject.value).to be 120
+    end
+  end
+
+  describe '#betterThan?' do
+    let(:rbi) { described_class.new('89') }
+
+    it 'should be better than rbi' do
+      expect(subject.betterThan?(rbi)).to be true
+    end
+  end
+
+  describe '#+' do
+    let(:other)   { described_class.new('89') }
+    let(:new_rbi) { subject + other           }
+
+    it 'should be an instance of described_class' do
+      expect(new_rbi).to be_instance_of described_class
+    end
+
+    it 'should have a value of the subject and other combined' do
+      expect(new_rbi.value).to be 209
+    end
+  end
 end
