7fdb5a2ea2c7ad38780e4b2b4e176d541140b839
Ryan Fisher <ryancfisher@gmail.com>
2015-02-15 19:33:56 -0800

    Add comparison and addition methods for Runs

--- lib/baseball/hitter/runs.rb
+++ lib/baseball/hitter/runs.rb
@@ -1,6 +1,18 @@
 module Baseball
   class Hitter
     class Runs < Baseball::Player::Stat
+      attr_reader :value
+      def initialize(runs)
+        @value = runs.to_i
+      end
+
+      def betterThan?(other)
+        value > other.value
+      end
+
+      def +(other)
+        self.class.new(value + other.value)
+      end
     end
   end
 end
--- spec/baseball/hitter/runs_spec.rb
+++ spec/baseball/hitter/runs_spec.rb
@@ -10,4 +10,31 @@ describe Baseball::Hitter::Runs do
       expect(subject).to be_instance_of described_class
     end
   end
+
+  describe '#value' do
+    it 'gives the value of an instance' do
+      expect(subject.value).to be 100
+    end
+  end
+
+  describe '#betterThan?' do
+    let(:runs) { described_class.new('200') }
+
+    it 'should be worse than runs' do
+      expect(subject.betterThan?(runs)).to be false
+    end
+  end
+
+  describe '#+' do
+    let(:other)    { described_class.new('187') }
+    let(:new_runs) { subject + other            }
+
+    it 'should be an instance of described_class' do
+      expect(new_runs).to be_instance_of described_class
+    end
+
+    it 'should have a value of the subject and other combined' do
+      expect(new_runs.value).to be 287
+    end
+  end
 end
