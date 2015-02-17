c29c5ac6b897f8c3d8471a31d8eba85da3e4b633
Ryan Fisher <ryancfisher@gmail.com>
2015-02-16 22:34:57 -0800

    Add value calculation for Obp

--- lib/baseball_stats/hitter/obp.rb
+++ lib/baseball_stats/hitter/obp.rb
@@ -1,6 +1,19 @@
 module BaseballStats
   class Hitter
     class Obp < BaseballStats::Player::Stat
+      attr_reader :hits, :bb, :ab, :hbp, :sf
+      def initialize(args)
+        @hits = args.fetch(:hits).to_i
+        @bb = args.fetch(:bb).to_i
+        @ab = args.fetch(:ab).to_i
+        @hbp = args.fetch(:hbp, 0).to_i
+        @sf = args.fetch(:sf, 0).to_i
+      end
+
+      def value
+        denom = ab + bb + hbp + sf
+        denom == 0 ? 0 : (hits + bb + hbp) / denom.to_f
+      end
     end
   end
 end
--- spec/baseball_stats/hitter/obp_spec.rb
+++ spec/baseball_stats/hitter/obp_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper'
 
 describe BaseballStats::Hitter::Obp do
-  let(:data) { '' }
+  subject { described_class.new(data) }
+
+  let(:data) { { hits: '0', bb: '0', ab: '0' } }
 
   describe '.new' do
-    subject { described_class.new(data) }
-
     it 'should return an instance of the class' do
       expect(subject).to be_instance_of described_class
+    end
+  end
+
+  describe '#value' do
+    context 'with default values' do
+      it 'should be a value of 0' do
+        expect(subject.value).to be 0
+      end
+    end
+
+    context 'with other values' do
+      let(:data) { { hits: '100', bb: '100', ab: '400' } }
+
+      it 'should be a value of .400' do
+        expect(subject.value).to be 0.400
+      end
     end
   end
 end
