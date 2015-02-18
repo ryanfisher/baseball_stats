7549d193632f99ca0d1c632bbb64be3f60c3d3a7
Ryan Fisher <ryancfisher@gmail.com>
2015-02-17 18:11:05 -0800

    Fix Slg class to holod correct values

--- lib/baseball_stats/hitter/slg.rb
+++ lib/baseball_stats/hitter/slg.rb
@@ -1,6 +1,12 @@
 module BaseballStats
   class Hitter
     class Slg < Stat
+      attr_reader :tb, :ab, :value
+      def initialize(args)
+        @tb = args.fetch(:tb).to_i
+        @ab = args.fetch(:ab).to_i
+        @value = tb / ab.to_f
+      end
     end
   end
 end
--- spec/baseball_stats/hitter/slg_spec.rb
+++ spec/baseball_stats/hitter/slg_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe BaseballStats::Hitter::Slg do
-  let(:data) { '' }
+  subject { described_class.new(data) }
+
+  let(:data) { {tb: '100', ab: '200'} }
 
   describe '.new' do
-    subject { described_class.new(data) }
-
     it 'should return an instance of the class' do
       expect(subject).to be_instance_of described_class
+    end
+  end
+
+  describe '#value' do
+    it 'should be .500' do
+      expect(subject.value).to be 0.500
     end
   end
 end
