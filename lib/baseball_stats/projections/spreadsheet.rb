6a6e963ff6de059cd3d8ac9ba873980e0398018f
Ryan Fisher <ryancfisher@gmail.com>
2015-02-20 16:21:18 -0800

    Add methods to split up sheet lines

--- lib/baseball_stats/projections/spreadsheet.rb
+++ lib/baseball_stats/projections/spreadsheet.rb
@@ -5,7 +5,25 @@ module BaseballStats
     # A projections spreadsheet
     class Spreadsheet
       def initialize(sheet)
-        @csv = CSV.new sheet
+        @csv = CSV.open(sheet, 'r:bom|utf-8').readlines
+        @headers = @csv[0]
+      end
+
+      def lines
+        # All csv lines after the header
+        @_lines ||= csv[1..-1].map { |line| player_hash line }
+      end
+
+      private
+
+      attr_reader :csv, :headers
+
+      def player_hash(line)
+        player = {}
+        line.each_with_index do |value, index|
+          player[headers[index]] = value
+        end
+        player
       end
     end
   end
--- spec/baseball_stats/projections/spreadsheet_spec.rb
+++ spec/baseball_stats/projections/spreadsheet_spec.rb
@@ -3,15 +3,41 @@ require 'spec_helper'
 describe BaseballStats::Projections::Spreadsheet do
   subject { described_class.new(sheet) }
 
-  let(:sheet) { '/file_location' }
+  let(:csv)     { double(:csv, readlines: sheet)             }
+  let(:hitter1) { ['Ryan Fisher', '.350', '45', '150', '55'] }
+  let(:hitter2) { ['Tony Gwynn', '.394', '12', '64', '5']    }
+  let(:sheet)   { [%w(name avg hr rbi sb), hitter1, hitter2] }
+
+  before do
+    expect(CSV).to receive(:open) { csv }
+  end
 
   describe '.new' do
-    before do
-      expect(CSV).to receive(:new).with(sheet)
-    end
-
     it 'should be an instance of the class' do
       expect(subject).to be_instance_of described_class
+    end
+  end
+
+  describe '#lines' do
+    it 'should return a line for each hitter' do
+      expect(subject.lines.length).to be 2
+    end
+
+    it 'should return an array of hashes that contain name keys' do
+      expect(subject.lines[0]['name']).to eq 'Ryan Fisher'
+      expect(subject.lines[1]['name']).to eq 'Tony Gwynn'
+    end
+
+    context 'checking memoization' do
+      let(:csv_obj) { double.as_null_object }
+
+      before do
+        expect(subject).to receive(:csv) { csv_obj }
+      end
+
+      it 'should only run calculations the first time lines is called' do
+        expect(subject.lines).to eq subject.lines
+      end
     end
   end
 end
