4b13dc78a2231a5163140a712ff511629ecf67bc
Ryan Fisher <ryancfisher@gmail.com>
2015-02-22 15:21:45 -0800

    Add methods to team class

--- lib/baseball_stats/fantasy/team.rb
+++ lib/baseball_stats/fantasy/team.rb
@@ -4,7 +4,22 @@ module BaseballStats
     class Team
       def initialize(args)
         @league = args.fetch(:league)
+        @hitters = []
+        @pitchers = []
       end
+
+      def add_hitter(hitter)
+        hitters << hitter
+      end
+
+      def add_pitcher(pitcher)
+        pitchers << pitcher
+      end
+
+      private
+
+      attr_reader :league
+      attr_accessor :hitters, :pitchers
     end
   end
 end
--- spec/baseball_stats/fantasy/team_spec.rb
+++ spec/baseball_stats/fantasy/team_spec.rb
@@ -3,11 +3,28 @@ require 'spec_helper'
 describe BaseballStats::Fantasy::Team do
   subject { described_class.new(data) }
 
-  let(:data) { { league: double(:league) } }
+  let(:data)   { { league: league } }
+  let(:league) { double(:league)    }
 
   describe '.new' do
     it 'is an intance of the described class' do
       expect(subject).to be_instance_of described_class
+    end
+  end
+
+  describe '#add_hitter' do
+    let(:hitter) { double(:hitter) }
+
+    it 'should add a hitter' do
+      expect(subject.add_hitter hitter).to eq [hitter]
+    end
+  end
+
+  describe '#add_pitcher' do
+    let(:pitcher) { double(:pitcher) }
+
+    it 'should add a pitcher' do
+      expect(subject.add_pitcher pitcher).to eq [pitcher]
     end
   end
 end
