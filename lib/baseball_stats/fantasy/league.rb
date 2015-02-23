638b4d179b149e17c9b560681d0aff00dbe8adb6
Ryan Fisher <ryancfisher@gmail.com>
2015-02-22 18:34:17 -0800

    Load fantasy teams from yaml

--- lib/baseball_stats/fantasy/league.rb
+++ lib/baseball_stats/fantasy/league.rb
@@ -1,9 +1,13 @@
+require 'yaml'
+
 module BaseballStats
   class Fantasy
     # A fantasy league containing a set of teams
     class League
+      attr_reader :teams
       def initialize(args)
         @projections = args[:projections]
+        @teams = YAML.load_file(args.fetch(:teams))
       end
     end
   end
--- spec/baseball_stats/fantasy/league_spec.rb
+++ spec/baseball_stats/fantasy/league_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe BaseballStats::Fantasy::League do
   subject { described_class.new(data) }
 
-  let(:data)        { { projections: projections } }
-  let(:projections) { double(:projections)         }
+  let(:data)        { { projections: projections, teams: teams } }
+  let(:projections) { double(:projections)                       }
+  let(:teams)       { 'data/team.yml'                            }
+
+  before do
+    expect(YAML).to receive(:load_file).with(teams)
+  end
 
   describe '.new' do
     it 'should be an instance of the described class' do
